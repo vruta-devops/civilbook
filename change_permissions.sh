35e5a60784848152f5a50b55c2d28daed7f86e3c
vruta-devops <vruta@moweb.in>
2024-10-04 16:54:56 +0530

    local

---
+++ change_permissions.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Change ownership to the Apache user (www-data)
+chown -R ubuntu:ubuntu /var/www/html
+
+# Set permissions to allow Apache to read the files
+chmod -R 755 /var/www/html
